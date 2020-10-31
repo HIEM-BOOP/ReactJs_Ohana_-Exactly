2b9a59772cd08e83e502e09fc123e01da68f1395
hiemkbr <hiemkbr@gmail.com>
2020-10-31 18:01:01 +0700

    cmd

--- src/components/ListRoom.tsx
+++ src/components/ListRoom.tsx
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import Room from './Room';
 import Search from './Search';
 
-class ListRoom extends Component {
+class ListRoom extends Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    var data = localStorage.getItem("dsPhong");
+    var dsphong = JSON.parse(data || "{}");
+    this.state = { rooms: dsphong, search: "" };
+  }
   render() {
+
     return (
       <div>
         <div className="listRoom">
@@ -11,14 +18,22 @@ class ListRoom extends Component {
           <div className="ketQua">
             <span className="fs-26">Kết quả</span>
           </div>
-          <Room></Room>
-          <Room></Room>
-          <Room></Room>
-          <Room></Room>
+          {
+            this.state.rooms.map((item: any) => {
+              return (
+                <Room></Room>
+                
+              )
+            })
+          }
         </div>
-      </div>
+      </div >
     );
   }
 }
+type State = {
+  rooms: any[];
+  search: string;
+};
 
 export default ListRoom;
--- src/model/Room.ts
+++ src/model/Room.ts
@@ -1,6 +1,10 @@
 export type Room = {
     title:string
-    
+    loaiPhong : string,
+    gioiTinh : string ,
+    dienTich : number,
+    diaChia : string ,
+    gia : number  
 }
 
 
